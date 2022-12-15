ca675a42e63c267bcc5a6981bc4c88dd3f006d6f
Pim Mostert <mostert.pim@gmail.com>
2022-12-15 13:56:17 +0100

    wip

--- azure_ml/component_preprocess_mnist.py
+++ azure_ml/component_preprocess_mnist.py
@@ -29,5 +29,8 @@ def preprocess_mnist_component(
         in one_hot(training_data_reshaped.T, 2).to(torch.float64)
     ]
     
+    # Save to store
     with open(output_file, 'wb') as file:
         pickle.dump(evidence, file)
+        
+    # Publish as data asset
--- azure_ml/pipeline_preprocess_mnist.py
+++ azure_ml/pipeline_preprocess_mnist.py
@@ -1,17 +1,15 @@
 from azure.ai.ml.dsl import pipeline
 from azure_ml.component_preprocess_mnist import preprocess_mnist_component
-from azure_ml.component_train import component_train
 
 # Create pipeline
 @pipeline(
     default_compute="gpu-cluster"
 )
-def pipeline_experiment(gamma: float):
+def pipeline_preprocess_mnist(gamma: float):
+    # Preprocess
     preprocess = preprocess_mnist_component(gamma=gamma)
     
-    train = component_train(evidence_file=preprocess.outputs.output_file)
-    
     return {
-        "bayesian_network_output_file": train.outputs.output_file
+        "bayesian_network_output_file": preprocess.outputs.output_file
     }
 
