0d6c1d9453f9e052c13c1c77627931b8fe0757dd
Pim Mostert <mostert.pim@gmail.com>
2024-11-08 21:49:02 +0100

    Try this

--- prefect/main.py
+++ prefect/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from prefect import flow
 from prefect.task_runners import ThreadPoolTaskRunner
 from bayesian_network.common.torch_settings import TorchSettings
@@ -8,8 +9,14 @@ from analyses.mnist.experiment import experiment
 
 
 @flow(task_runner=ThreadPoolTaskRunner(max_workers=10))
-def main():
+async def main():
     torch_settings = TorchSettings(torch.device("cpu"), torch.float64)
-    selected_num_observations = 1000
 
-    experiment(torch_settings, selected_num_observations)
+    await asyncio.gather(
+        experiment(torch_settings, selected_num_observations=1000),
+        experiment(torch_settings, selected_num_observations=2000),
+        experiment(torch_settings, selected_num_observations=3000),
+        experiment(torch_settings, selected_num_observations=4000),
+        experiment(torch_settings, selected_num_observations=5000),
+        experiment(torch_settings, selected_num_observations=6000),
+    )
