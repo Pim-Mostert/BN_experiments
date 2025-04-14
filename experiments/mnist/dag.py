512c21b656b500a824e092df4c702211ef342d29
Pim Mostert <mostert.pim@gmail.com>
2025-04-14 21:35:17 +0200

    Use EmBatchOptimizer in MNIST experiment

--- experiments/mnist/dag.py
+++ experiments/mnist/dag.py
@@ -1,6 +1,9 @@
+from dataclasses import asdict
 from pathlib import Path
 from airflow.models.param import Param
 from dags_common import create_experiment_dag
+from bayesian_network.common.torch_settings import TorchSettings
+from bayesian_network.optimizers.em_batch_optimizer import EmBatchOptimizerSettings
 
 experiment_id = Path(__file__).parents[0].stem
 notebook_path = Path(__file__).parents[0] / f"{experiment_id}.py"
@@ -9,14 +12,23 @@ dag = create_experiment_dag(
     experiment_id,
     notebook_path,
     experiment_params={
-        "device": Param(
-            default="cpu",
-            type="string",
-            enum=["cpu", "cuda", "mps"],
-            description="PyTorch device",
+        "TORCH_SETTINGS": Param(
+            default=dict(TorchSettings(device="cpu", dtype="float64")),
+            type="object",
         ),
-        "selected_num_observations": Param(1000, type="integer"),
-        "num_iterations": Param(10, type="integer"),
-        "gamma": Param(0.00001, type="number"),
+        "EM_BATCH_OPTIMIZER_SETTINGS": Param(
+            default=asdict(EmBatchOptimizerSettings(num_iterations=100, learning_rate=0.01)),
+            type="object",
+        ),
+        "SELECTED_NUM_OBSERVATIONS": Param(
+            default=2000,
+            type="integer",
+            description="Select subset of MNIST dataset",
+        ),
+        "GAMMA": Param(
+            default=0.001,
+            type="number",
+            description="Normalization factor of MNIST data",
+        ),
     },
 )
