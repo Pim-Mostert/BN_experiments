81fa4515cb9d8e3a455c679b310828cbcf8b382f
Pim Mostert <mostert.pim@gmail.com>
2025-04-16 23:07:24 +0200

    Implement MnistEvidenceBatches and update mnist experiment

--- experiments/mnist/dag.py
+++ experiments/mnist/dag.py
@@ -1,6 +1,9 @@
+from dataclasses import asdict
 from pathlib import Path
 from airflow.models.param import Param
 from dags_common import create_experiment_dag
+from bayesian_network.common.torch_settings import TorchSettings
+from bayesian_network.optimizers.em_batch_optimizer import EmBatchOptimizerSettings
 
 experiment_id = Path(__file__).parents[0].stem
 notebook_path = Path(__file__).parents[0] / f"{experiment_id}.py"
@@ -9,37 +12,17 @@ dag = create_experiment_dag(
     experiment_id,
     notebook_path,
     experiment_params={
-        "DEVICE": Param(
-            default="cpu",
-            type="string",
-            enum=["cpu", "cuda", "mps"],
-            description="PyTorch device",
-        ),
-        "DTYPE": Param(
-            default="float64",
-            type="string",
-            enum=["float64", "float32"],
-            description="PyTorch dtype",
-        ),
-        "NUM_ITERATIONS": Param(
-            default=100,
-            type="integer",
-            description="Number of iterations for EM Batch Optimizer",
-        ),
-        "LEARNING_RATE": Param(
-            default=0.01,
-            type="number",
-            description="Learning rate for EM Batch Optimizer",
+        "TORCH_SETTINGS": Param(
+            default=dict(TorchSettings(device="cpu", dtype="float64")),
+            type="object",
         ),
         "BATCH_SIZE": Param(
             default=50,
             type="integer",
-            description="Batch size for EM Batch Optimizer",
         ),
-        "SELECTED_NUM_OBSERVATIONS": Param(
-            default=2000,
-            type="integer",
-            description="Select subset of MNIST dataset",
+        "EM_BATCH_OPTIMIZER_SETTINGS": Param(
+            default=asdict(EmBatchOptimizerSettings(num_iterations=100, learning_rate=0.01)),
+            type="object",
         ),
         "GAMMA": Param(
             default=0.001,
