e99362851716a1b9edc68ed3bfac399d4749d839
Pim Mostert <mostert.pim@gmail.com>
2026-01-06 22:54:32 +0100

    wip

--- experiments/mnist/_008_Stride_and_kernel_size/008-Stride_and_kernel_size.py
+++ experiments/mnist/_008_Stride_and_kernel_size/008-Stride_and_kernel_size.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import mlflow
 import torch
 import torchvision
+from bayesian_network.samplers.torch_sampler import TorchBayesianNetworkSampler
 from bayesian_network.bayesian_network import Node
 from bayesian_network.common.torch_settings import TorchSettings
 from bayesian_network.inference_machines.evidence import Evidence, EvidenceLoader
@@ -18,11 +19,9 @@ from bayesian_network.optimizers.em_batch_optimizer import (
     EmBatchOptimizer,
     EmBatchOptimizerSettings,
 )
-from bayesian_network.samplers.torch_sampler import TorchBayesianNetworkSampler
 from torch.utils.data import DataLoader, Subset
 from torchvision import transforms
-
-from dev_experiments.networks import Network4LBuilder
+from experiments.mnist._008_Stride_and_kernel_size.network import Network3LBuilder
 from experiments.mnist.common import (
     MLflowOptimizerLogger,
 )
@@ -56,7 +55,7 @@ TORCH_SETTINGS = TorchSettings(
 BATCH_SIZE = 100
 LEARNING_RATE = 0.1
 REGULARIZATION = 0.001
-NUM_EPOCHS = 3
+NUM_EPOCHS = 1
 
 mlflow.log_param("TORCH_SETTINGS", TORCH_SETTINGS)
 mlflow.log_param("BATCH_SIZE", BATCH_SIZE)
@@ -67,17 +66,18 @@ mlflow.log_param("NUM_EPOCHS", NUM_EPOCHS)
 
 # %% Load data
 
-height, width = 28, 28
+height, width = 22, 22
 
-transforms = transforms.Compose(
+mnist_transform = transforms.Compose(
     [
         transforms.ToTensor(),
+        transforms.Lambda(lambda x: x[:, 3:25, 3:25]),
         transforms.Lambda(lambda x: x.flatten()),
     ],
 )
 
 mnist = torchvision.datasets.MNIST(
-    "./experiments/mnist", train=True, download=True, transform=transforms
+    "./experiments/mnist", train=True, download=True, transform=mnist_transform
 )
 mnist = Subset(mnist, range(5000))
 
@@ -115,25 +115,15 @@ evidence_loader = EvidenceLoader(
 )
 
 # %% Define network
+numF0 = 50
+numF1 = 3
+numF2 = 5
 
-# 			                 F0
-#        F1		             F1	                  F1
-#   F2	      F2  	   F2    F2    F2	      F2	  F2
-# F3  F3	F3  F3	  F3 F3 F3 F3 F3 F3     F3  F3  F3  F3
-# Y Y Y Y	Y Y Y Y	  YY YY YY YY YY YY	    Y Y Y Y	Y Y Y Y
-
-num_F0 = 20
-num_F1 = 20
-num_F2 = 20
-num_F3 = 20
-
-# network, F0, Ys = Network1LBuilder(TORCH_SETTINGS, 28, 28).build(num_F0)
-# network, F0, F1s, Ys = Network2LBuilder(TORCH_SETTINGS, 28, 28).build(num_F0, num_F1)
-# network, F0, F1s, F2s, Ys = Network3LBuilder(TORCH_SETTINGS, 28, 28).build(num_F0, num_F1, num_F2)
-network, F0, F1s, F2s, F3s, Ys = Network4LBuilder(TORCH_SETTINGS, 28, 28).build(
-    num_F0, num_F1, num_F2, num_F3
+network, F0, F1s, F2s, Ys = Network3LBuilder(TORCH_SETTINGS).build(
+    numF0=numF0,
+    numF1=numF1,
+    numF2=numF2,
 )
-
 
 logging.info("Network degrees of freedom: %s", network.degrees_of_freedom)
 mlflow.log_metric("DoF", network.degrees_of_freedom)
@@ -155,6 +145,8 @@ def create_inference_machine_factory(num_observations):
             settings=SpaInferenceMachineSettings(
                 torch_settings=TORCH_SETTINGS,
                 average_log_likelihood=True,
+                allow_loops=True,
+                num_iterations=10,
             ),
             bayesian_network=network,
             observed_nodes=Ys,
@@ -199,6 +191,8 @@ def get_reconstruction(observed_nodes: List[Node], queries: Evidence):
         settings=SpaInferenceMachineSettings(
             TORCH_SETTINGS,
             average_log_likelihood=True,
+            allow_loops=True,
+            num_iterations=10,
         ),
         bayesian_network=network,
         num_observations=queries.num_observations,
@@ -213,8 +207,8 @@ def get_reconstruction(observed_nodes: List[Node], queries: Evidence):
 
 # %% Root template
 
-queries = Evidence([F0.cpt], torch_settings=TORCH_SETTINGS)
-template = get_reconstruction([F0], queries)[0]
+queries = Evidence([F0.cpt[None, :]], torch_settings=TORCH_SETTINGS)
+template = get_reconstruction([F0], queries).squeeze()
 
 figure = plt.figure()
 figure.suptitle("Grand mean")
@@ -225,52 +219,43 @@ mlflow.log_figure(figure, "Templates_root.png")
 
 # %% F0 templates
 
-queries = Evidence([torch.eye(num_F0)], torch_settings=TORCH_SETTINGS)
+queries = Evidence([torch.eye(numF0)], torch_settings=TORCH_SETTINGS)
 templates = get_reconstruction([F0], queries)
 
 plt.figure()
-plt.suptitle("Q templates")
-for i in range(num_F0):
-    plt.subplot(int(num_F0**0.5) + 1, int(num_F0**0.5) + 1, i + 1)
+plt.suptitle("F0 templates")
+for i in range(numF0):
+    plt.subplot(int(numF0**0.5) + 1, int(numF0**0.5) + 1, i + 1)
     plot(templates[i].reshape(height, width))
 
 mlflow.log_figure(figure, "Templates_F0.png")
 
 # %% F1 templates
 
-if1 = 4
-queries = Evidence([torch.eye(num_F1)], torch_settings=TORCH_SETTINGS)
-templates = get_reconstruction([F1s[if1]], queries)
+iF1 = 5
 
-for i in range(0, num_F1):
-    plt.subplot(5, 4, i + 1)
+queries = Evidence([torch.eye(numF1)], torch_settings=TORCH_SETTINGS)
+templates = get_reconstruction([F1s[iF1]], queries)
+
+for i in range(0, numF1):
+    plt.subplot(1, 5, i + 1)
     plot(templates[i].reshape(height, width))
 
 mlflow.log_figure(figure, "Templates_F1.png")
 
 # %% F2 templates
 
-if2 = 24
-queries = Evidence([torch.eye(num_F2)], torch_settings=TORCH_SETTINGS)
-templates = get_reconstruction([F2s[if2]], queries)
+iF2 = 55
 
-for j in range(0, num_F2):
-    plt.subplot(5, 4, j + 1)
-    plot(templates[j].reshape(height, width))
+queries = Evidence([torch.eye(numF2)], torch_settings=TORCH_SETTINGS)
+templates = get_reconstruction([F2s[iF2]], queries)
+
+for i in range(0, numF2):
+    plt.subplot(1, 5, i + 1)
+    plot(templates[i].reshape(height, width))
 
 mlflow.log_figure(figure, "Templates_F2.png")
 
-# %% F3 templates
-
-if3 = 100
-queries = Evidence([torch.eye(num_F3)], torch_settings=TORCH_SETTINGS)
-templates = get_reconstruction([F3s[if3]], queries)
-
-for j in range(0, num_F3):
-    plt.subplot(5, 4, j + 1)
-    plot(templates[j].reshape(height, width))
-
-mlflow.log_figure(figure, "Templates_F3.png")
 
 # %% Sample reconstruction
 
@@ -286,7 +271,6 @@ inference_machine.enter_evidence(evidence)
 pF0 = inference_machine.infer_single_nodes([F0])
 pF1s = inference_machine.infer_single_nodes(F1s)
 pF2s = inference_machine.infer_single_nodes(F2s)
-pF3s = inference_machine.infer_single_nodes(F3s)
 
 ### P(Y | F0)
 y_hat_F0 = get_reconstruction([F0], Evidence(pF0, TORCH_SETTINGS))
@@ -294,11 +278,8 @@ y_hat_F0 = get_reconstruction([F0], Evidence(pF0, TORCH_SETTINGS))
 ### P(Y | F1)
 y_hat_F1 = get_reconstruction(F1s, Evidence(pF1s, TORCH_SETTINGS))
 
-### P(Y | F2)
+### P(Y | F1)
 y_hat_F2 = get_reconstruction(F2s, Evidence(pF2s, TORCH_SETTINGS))
-
-### P(Y | F3)
-y_hat_F3 = get_reconstruction(F3s, Evidence(pF3s, TORCH_SETTINGS))
 
 ### Plot
 figure = plt.figure(figsize=(6, 50))
@@ -306,31 +287,26 @@ for i, x in enumerate(X):
     pfs = [e[i][None, :] for e in pF1s]
 
     # Sample
-    ax1 = plt.subplot(len(X), 5, i * 5 + 1)
+    ax1 = plt.subplot(len(X), 4, i * 4 + 1)
     plot(x.reshape(height, width))
 
     # From F0
-    ax2 = plt.subplot(len(X), 5, i * 5 + 2)
+    ax2 = plt.subplot(len(X), 4, i * 4 + 2)
     plot(y_hat_F0[i].reshape(height, width))
 
     # From F1
-    ax3 = plt.subplot(len(X), 5, i * 5 + 3)
+    ax3 = plt.subplot(len(X), 4, i * 4 + 3)
     plot(y_hat_F1[i].reshape(height, width))
 
     # From F2
-    ax4 = plt.subplot(len(X), 5, i * 5 + 4)
+    ax3 = plt.subplot(len(X), 4, i * 4 + 4)
     plot(y_hat_F2[i].reshape(height, width))
-
-    # From F3
-    ax5 = plt.subplot(len(X), 5, i * 5 + 5)
-    plot(y_hat_F3[i].reshape(height, width))
 
     if i == 0:
         ax1.set_title("Sample")
         ax2.set_title("From F0")
         ax3.set_title("From F1")
-        ax4.set_title("From F2")
-        ax4.set_title("From F3")
+        ax3.set_title("From F2")
 
 mlflow.log_figure(figure, "Reconstructions.png")
 
@@ -346,5 +322,7 @@ for i, x in enumerate(data):
     plot(x.reshape(height, width))
 
 mlflow.log_figure(figure, "Generated_samples.png")
+
+# %%
 
 # %%
